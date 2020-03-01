5fbce8c2e0650be8f26d16ec0fee0efdd959b9cc
Rouan Thompson <rouan.thompson@gmail.com>
2020-03-01 03:03:38 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
+<<<<<<< HEAD
 def parrot(string = "Squawk!")
+=======
+def parrot(string = "Squawk")
+>>>>>>> a02af73b0e83f5a674c8fa0982b8acd34f3c5700
   puts "#{string}"
   string
 end
